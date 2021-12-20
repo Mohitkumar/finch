1940afde7a951aaa417dbbc862975f9fa4507f10
mohit <ch.mohitkumar89@gmail.com>
2021-12-20 09:29:51 +0530

    log package

--- log/log.go
+++ log/log.go
@@ -1,6 +1,12 @@
 package log
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 
 	api "github.com/mohitkumar/finch/api/v1"
@@ -35,17 +41,98 @@ func NewLog(dir string, c Config) (Log, error) {
 		Dir:    dir,
 		Config: c,
 	}
-	return l, nil
+	return l, l.setup()
+}
+
+func (log *logImpl) setup() error {
+	files, err := ioutil.ReadDir(log.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffsets []uint64
+	for _, file := range files {
+		offsetStr := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
+		off, _ := strconv.ParseUint(offsetStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+	for i := 0; i < len(baseOffsets); i++ {
+		if err := log.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+		i++
+	}
+	if log.segments == nil {
+		if err := log.newSegment(log.Config.Segment.InitialOffset); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (log *logImpl) newSegment(offset uint64) error {
+	s, err := newSegment(log.Dir, offset, log.Config)
+	if err != nil {
+		return err
+	}
+	log.segments = append(log.segments, s)
+	log.activeSegment = s
+	return nil
 }
 
 func (log *logImpl) Append(record *api.LogRecord) (uint64, error) {
-
+	log.mu.Lock()
+	defer log.mu.Unlock()
+	off, err := log.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+	if log.activeSegment.IsFull() {
+		err = log.newSegment(off + 1)
+	}
+	return off, err
 }
 
 func (log *logImpl) Read(offset uint64) (*api.LogRecord, error) {
-
+	log.mu.Lock()
+	defer log.mu.Unlock()
+	var s *segment
+	for _, segment := range log.segments {
+		if offset >= segment.baseOffset && offset < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+	if s == nil || s.nextOffset <= offset {
+		return nil, api.ErrOffsetOutOfRange{Offset: offset}
+	}
+	// END: after
+	return s.Read(offset)
 }
 
-func (log *logImpl) Close() error {
+func (l *logImpl) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
+func (l *logImpl) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(l.Dir)
+}
+
+func (l *logImpl) Reset() error {
+	if err := l.Remove(); err != nil {
+		return err
+	}
+	return l.setup()
 }
